90c2d454bbd1d74e44df9cbc60126707d7e2912e
bryanedds <bryanedds@gmail.com>
2022-01-07 15:57:10 -0500

    Better Unit type.

--- Sigma/Union2.cs
+++ Sigma/Union2.cs
@@ -1265,8 +1265,15 @@ namespace Sigma.Union2
 		public static Overload9 Value => new Overload9();
 	}
 
-	public struct Unit
+	[Serializable]
+	public struct Unit : IEquatable<Unit>
 	{
-		public static Unit Value => new Unit(); // NOTE: BGE: assuming that construction costs are zero.
+		public static readonly Unit Value = new Unit();
+		public static bool operator ==(Unit first, Unit second) => true;
+		public static bool operator !=(Unit first, Unit second) => false;
+		public bool Equals(Unit other) => true;
+		public override bool Equals(object obj) => obj is Unit;
+		public override int GetHashCode() => 0;
+		public override string ToString() => "()";
 	}
 }
